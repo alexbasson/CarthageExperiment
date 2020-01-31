a14cf4ef27bfe638ac4f9cb97b189157a2971583
Alex Basson <alexbasson@gmail.com>
2020-01-31 14:13:23 -0500

    Initial Commit

---
+++ CarthageExperiment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarthageExperiment
+//
+//  Created by Alex on 1/31/20.
+//  Copyright © 2020 Conditionally Convergent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
